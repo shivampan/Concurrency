64bb50a216eebe5c4c703ec47ad42cbc94750caf
shivampan <shivampwork12012000@gmail.com>
2024-09-03 19:42:22 +0530

    Syntax error fixed

--- src/main/java/com/shivam/concurreny2printnumber1to100/Main.java
+++ src/main/java/com/shivam/concurreny2printnumber1to100/Main.java
@@ -1,2 +1,11 @@
-package com.shivam.concurreny2printnumber1to100;public class Main {
+package com.shivam.concurreny2printnumber1to100;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i=1;i<=100;i++){
+            NumberPrinter numberPrinter=new NumberPrinter(i);
+            Thread thread=new Thread(numberPrinter);
+            thread.start();
+        }
+    }
 }
--- src/main/java/com/shivam/concurreny2printnumber1to100/NumberPrinter.java
+++ src/main/java/com/shivam/concurreny2printnumber1to100/NumberPrinter.java
@@ -1,2 +1,12 @@
-package com.shivam.concurreny2printnumber1to100;public class NumberPrinter {
+package com.shivam.concurreny2printnumber1to100;
+
+public class NumberPrinter implements Runnable {
+   private int number;
+   public NumberPrinter(int number) {
+       this.number = number;
+   }
+   public void run() {
+       System.out.println(number + "-Thread name: "+Thread.currentThread().getName());
+
+   }
 }
